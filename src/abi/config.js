f313435bede520bdfaa4a680a7b66d51d260e757
abdec2 <abdec2@hotmail.com>
2022-06-14 06:28:16 +0300

    Update config.js

--- src/abi/config.js
+++ src/abi/config.js
@@ -1,7 +1,8 @@
 
 const CONFIG = {
     chainId: 4,
-    contractAddress: "0x3572aE635382DdBD2ee142D9bcfc834dE7dCBD09", 
+    // contractAddress: "0x3572aE635382DdBD2ee142D9bcfc834dE7dCBD09", 
+    contractAddress: "0xcFb3b7844eBCeaf99B5C4c6deC10378f3a719Ab7",
     tokenAddress: "0xfd259e4FB9C6722e76FEdc7D38EBB6C31f14b9F7", 
     tokenDecimals: '18',
     tokenSymbol: 'QTH', 
